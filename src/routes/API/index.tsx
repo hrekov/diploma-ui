2e456adaafa79108b797a1a58f223354755c988c
Oleksandr Hrekov <oleksandrhrekov@icloud.com>
2023-02-27 20:21:41 +0200

    Add FAQ page with stub data

--- src/routes/API/index.tsx
+++ src/routes/API/index.tsx
@@ -1,7 +1,105 @@
 import React from 'react';
+import SectionTitle from '../../components/SectionTitle';
+import css from './style.module.scss';
 
 function APIPage() {
-  return <></>;
+  return (
+    <div>
+      <SectionTitle title="API Documentation" className={css.title}/>
+
+      <p className={css.note}>
+        NOTE: Current API doesn't require authorization to work with it. This
+        service
+        provides REST
+      </p>
+
+      <section>
+        <h2>Endpoints</h2>
+        <table>
+          <thead>
+          <tr>
+            <th>Endpoint</th>
+            <th>Description</th>
+          </tr>
+          </thead>
+          <tbody>
+          <tr>
+            <td>/api/v1/health</td>
+            <td>Get service health</td>
+          </tr>
+          <tr>
+            <td>/api/v1/recognize</td>
+            <td>Perform recognition on provided image</td>
+          </tr>
+          </tbody>
+        </table>
+      </section>
+
+      <section className={css.endpointSection}>
+        <h2>GET /api/v1/health</h2>
+        <p>Returns a list of all users.</p>
+        <h3>Example Request</h3>
+        <pre>
+          GET /api/users HTTP/1.1
+          Host: api.example.com
+        </pre>
+
+        <h3>Example Response</h3>
+
+        <pre>
+        HTTP/1.1 200 OK
+        Content-Type: application/json
+
+          {`
+      [
+          {
+            "id": 1,
+            "name": "John Doe",
+            "email": "john.doe@example.com"
+          },
+          {
+            "id": 2,
+            "name": "Jane Smith",
+            "email": "jane.smith@example.com"
+          }
+      ]
+      `}
+    </pre>
+      </section>
+
+      <section className={css.endpointSection}>
+        <h2>POST /api/v1/recognize</h2>
+        <p>Returns a list of all users.</p>
+        <h3>Example Request</h3>
+        <pre>
+          GET /api/users HTTP/1.1
+          Host: api.example.com
+        </pre>
+
+        <h3>Example Response</h3>
+
+        <pre>
+        HTTP/1.1 200 OK
+        Content-Type: application/json
+
+          {`
+      [
+          {
+            "id": 1,
+            "name": "John Doe",
+            "email": "john.doe@example.com"
+          },
+          {
+            "id": 2,
+            "name": "Jane Smith",
+            "email": "jane.smith@example.com"
+          }
+      ]
+      `}
+    </pre>
+      </section>
+    </div>
+  );
 }
 
 export default APIPage;
--- src/routes/FAQ/index.tsx
+++ src/routes/FAQ/index.tsx
@@ -3,9 +3,25 @@ import css from './style.module.scss';
 
 function FAQ() {
   return (
-    <>
-
-    </>
+    <div className={css.container}>
+      <h1>Frequently Asked Questions</h1>
+      <h2>How do I upload an image of my car to a website for recognition?</h2>
+      <p>
+        To upload an image of your car to a website for recognition, you'll typically need to click on a button or link that says something like "Upload Image" or "Choose File". This will open a dialog box that allows you to select the image file from your computer. After you've selected the file, click on the "Upload" or "Submit" button to upload the image to the website's server for recognition.
+      </p>
+      <h2>Is it safe to upload images of my car to a website?</h2>
+      <p>
+        It's generally safe to upload images of your car to a website, as long as you trust the website and its security measures. Look for a lock icon in the address bar of your browser to confirm that the connection is secure. Also, make sure you trust the website before uploading any sensitive images, as they may be stored on the website's server.
+      </p>
+      <h2>What format should my image be in?</h2>
+      <p>
+        This website accepts common image formats such as JPEG, PNG, and GIF
+      </p>
+      <h2>How do I know if my image has been successfully uploaded?</h2>
+      <p>
+        Once you have selected your image, the website will display you a preview of the image or a message confirming that the upload was successful. If you don't see any confirmation message or preview, try refreshing the page or checking your internet connection.
+      </p>
+    </div>
   );
 }
 
